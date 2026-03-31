97f358ea70ee797b46c80ef91c6cedb6130cc2f0
shikhavnipatel-lab <shikhavnipatel@gmail.com>
2026-03-31 16:06:42 +0530

    QUEUE

---
+++ queue.py
@@ -0,0 +1,11 @@
+queue = []
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+print("Queue after enqueue:", queue)
+# Dequeue
+queue.pop(0)
+print("Queue after dequeue:", queue)
+# Front element
+print("Front element:", queue[0])
